11b50ff095b8ec2ee4e5bc50eaa101e5655cef85
VladBeglik <beglikvv@belhard.com>
2023-04-05 01:50:34 +0300

    fix

--- CarService.API/Program.cs
+++ CarService.API/Program.cs
@@ -1,11 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Reflection;
+using CarService.App.Infrastructure;
+using CarService.Persistance;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IServiceDbContext, ServiceDbContext>();
 
 var app = builder.Build();
 
